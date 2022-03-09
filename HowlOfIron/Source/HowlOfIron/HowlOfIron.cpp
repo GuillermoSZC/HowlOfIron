fbe3a8d4844d87305a2952af50992a99ddc0c547
Marco Fercia <marcofernandezgarciaa@gmail.com>
2022-03-09 13:29:28 +0100

    borrado

--- HowlOfIron/Source/HowlOfIron/HowlOfIron.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HowlOfIron.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HowlOfIron, "HowlOfIron" );
- 
